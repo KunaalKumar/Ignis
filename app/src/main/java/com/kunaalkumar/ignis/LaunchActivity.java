a6c4845d235fa269a2601d92316c1482335279a6
kunaalkumar <kunaal@ashiana.com>
2018-07-01 10:42:51 -0700

    Add splash screen

---
+++ app/src/main/java/com/kunaalkumar/ignis/LaunchActivity.java
@@ -0,0 +1,18 @@
+package com.kunaalkumar.ignis;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}

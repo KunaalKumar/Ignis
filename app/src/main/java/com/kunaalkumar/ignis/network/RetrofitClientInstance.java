00171514e0498773efbf6e8c305dbf670c656c1e
kunaalkumar <kunaal@ashiana.com>
2018-07-02 10:03:04 -0700

    Change text to hint for MainActivity XML

--- app/src/main/java/com/kunaalkumar/ignis/network/RetrofitClientInstance.java
+++ app/src/main/java/com/kunaalkumar/ignis/network/RetrofitClientInstance.java
@@ -1,7 +1,21 @@
 package com.kunaalkumar.ignis.network;
 
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientInstance {
+
     private static Retrofit retrofit;
+    private static final String BASE_URL = "http://superheroapi.com/api/10205048573171973";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
 }

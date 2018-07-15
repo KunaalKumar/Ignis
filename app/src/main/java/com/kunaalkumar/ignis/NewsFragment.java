8d26bcd5f946fa56dd8f880b66e08344ba1312ae
kunaalkumar <kunaal@ashiana.com>
2018-07-15 11:56:09 -0700

    Implement news and favorites fragments, layouts and main activity functionality for switching layouts

---
+++ app/src/main/java/com/kunaalkumar/ignis/NewsFragment.java
@@ -0,0 +1,17 @@
+package com.kunaalkumar.ignis;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class NewsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/kunaalkumar/ignis/activities/MainActivity.java
+++ app/src/main/java/com/kunaalkumar/ignis/activities/MainActivity.java
@@ -10,24 +10,19 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.kunaalkumar.ignis.BottomBarFragment;
+import com.kunaalkumar.ignis.FavoritesFragment;
+import com.kunaalkumar.ignis.NewsFragment;
 import com.kunaalkumar.ignis.R;
 import com.kunaalkumar.ignis.SharedPrefs;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String FORMAT = "json";
 
-    private List<BottomBarFragment> fragments = new ArrayList<>(2);
-
-    private static final String TAG_FRAGMENT_NEWS = "tag_frag_news";
-    private static final String TAG_FRAGMENT_FAVORITES = "tag_favorites";
+    private NewsFragment newsFragment = new NewsFragment();
+    private FavoritesFragment favoritesFragment = new FavoritesFragment();
 
     // Api key for ComicVine
     public static final String API_KEY = "9aa1dc67801a2cdc8460790837f94b73057ce351";
@@ -51,9 +46,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        initBottomNav();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_fragmentholder, newsFragment)
+                .commit();
 
-        switchFragment(0, TAG_FRAGMENT_NEWS);
+        initBottomNav();
 
     }
 
@@ -70,46 +68,28 @@ public class MainActivity extends AppCompatActivity {
 
     // handle bottom navigation events
     private void initBottomNav() {
-        buildFragmentsList();
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
+
                     case R.id.bottom_navigation_news:
-                        switchFragment(0, TAG_FRAGMENT_NEWS);
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frame_fragmentholder, newsFragment)
+                                .commit();
                         return true;
+
                     case R.id.bottom_navigation_favorites:
-                        switchFragment(1, TAG_FRAGMENT_FAVORITES);
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.frame_fragmentholder, favoritesFragment)
+                                .commit();
                         return true;
+
                 }
                 return false;
             }
         });
-    }
-
-    // Switch fragment in frame layout
-    private void switchFragment(int pos, String tag) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.frame_fragmentholder, fragments.get(pos), tag)
-                .commit();
-    }
-
-    // Builds fragments to list
-    private void buildFragmentsList() {
-        BottomBarFragment callsFragment = buildFragment("News");
-        BottomBarFragment recentsFragment = buildFragment("Favorites");
-
-        fragments.add(callsFragment);
-        fragments.add(recentsFragment);
-    }
-
-    // Returns BottomBarFragment whilst passing title as argument
-    private BottomBarFragment buildFragment(String title) {
-        BottomBarFragment fragment = new BottomBarFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString(BottomBarFragment.ARG_NAME, title);
-        fragment.setArguments(bundle);
-        return fragment;
     }
 }
